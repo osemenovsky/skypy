60bb1953f677c69f65cce3286fc8ca876db82905
Ivan Brezhnev <brezhnev.ivan@yahoo.com>
2016-05-08 23:26:01 +0300

    Fix error when get registration token

--- skypy.py
+++ skypy.py
@@ -2,17 +2,17 @@
 
 # Copyright (C) 2015  Semenovsky, Oleg <o.semenovsky@gmail.com>
 # Author: Semenovsky, Oleg <o.semenovsky@gmail.com>
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -291,7 +291,7 @@ class Skype():
         # Parsing a registrationToken from response
         registration_token_raw = response.headers["set-registrationtoken"]
         for line in registration_token_raw.split("; "):
-            k, v = line.split("=")
+            k, v = line.split("=", 1)
             self.registration_token[k] = v
 
         return self.registration_token["registrationToken"]
